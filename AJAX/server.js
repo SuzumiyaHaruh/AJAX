3a029e1d4b1bdbc9c2b94b3856bd52ac2f5b4db2
SuzumiyaHaruh <3563692396@qq.com>
2022-04-21 22:20:00 +0800

    更新

--- AJAX/server.js
+++ AJAX/server.js
@@ -5,28 +5,39 @@ const app = express();
 // 创建路由规则
 // request 是对请求报文的封装
 // response 是对响应报文封装
+// json
 app.get('/json-server', (request, response) => {
     {
         // 设置响应头,设置允许跨域
         response.setHeader('Access-Control-Allow-Origin', '*');
         const data={
-            name:'huang'
+            name:'json'
         }
         // 设置响应
         response.send(data);
     }
 });
+// Get请求向服务器索取数据
 app.get('/server', (request, response) => {
     {
         // 设置响应头,设置允许跨域
         response.setHeader('Access-Control-Allow-Origin', '*');
-        const data={
-            name:'huang'
-        }
         // 设置响应
-        response.send(data);
+        response.send('HELLO GET');
     }
 });
+//延时
+app.get('/time-server', (request, response) => {
+    {
+        // 设置响应头,设置允许跨域
+        response.setHeader('Access-Control-Allow-Origin', '*');
+        // 加一个定时器用于判断超时
+        setTimeout(()=>{
+            response.send('delayed response');
+        },3000);
+    }
+});
+//针对ie缓存
 app.get('/ie', (request, response) => {
     {
         // 设置响应头,设置允许跨域
@@ -38,12 +49,22 @@ app.get('/ie', (request, response) => {
         response.send(data);
     }
 });
+// 可以接收任何类型请求
+app.all('/ie', (request, response) => {
+    {
+        // 设置响应头,设置允许跨域
+        response.setHeader('Access-Control-Allow-Origin', '*');
+        // 设置响应
+        response.send('HELLO ALL');
+    }
+});
+//Post请求向服务器提交数据
 app.post('/server', (request, response) => {
     {
         // 设置响应头,设置允许跨域
         response.setHeader('Access-Control-Allow-Origin', '*');
         // 设置响应
-        response.send('HELLO EXPRESS');
+        response.send('HELLO POST');
     }
 });
 // 监听端口启动
